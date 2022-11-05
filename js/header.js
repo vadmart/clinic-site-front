2b790fc7c8eb52dfd6a244eac150204638e1d121
Vadim Martov <z9661744@gmail.com>
2022-11-06 00:41:21 +0200

    Adaptive pictures in staff page. Regional clinic website

---
+++ js/header.js
@@ -0,0 +1,23 @@
+// const menu = document.getElementsByClassName("menu")[0];
+// const menu_for_change = document.getElementsByClassName("menu")[0];
+//
+// window.addEventListener("resize", function (event) {
+//     const menu_elements = document.getElementsByClassName("menu-item");
+//     const window_width = window.screen.width;
+//     const btn = document.createElement("button");
+//     if (window_width <= 768) {
+//         btn.id = "mobile_btn";
+//         for (var el of menu_elements) {
+//             el.style.display = "none";
+//         }
+//         menu_for_change.style.alignItems = "flex-start";
+//         if (!document.getElementById("mobile_btn")) {
+//             menu.appendChild(btn);
+//         }
+//     } else {
+//         for (var elem of menu_elements) {
+//             elem.style.display = "";
+//         }
+//         document.removeChild(btn);
+//     }
+// })
