069d4445a8bd7612d019a70f146c201389ded5d0
vadjango <z9661744@gmail.com>
2022-12-02 17:09:55 +0200

    Completed validation. Modal window (made on clear js) in process

--- js/form.js
+++ js/form.js
@@ -1,60 +1,93 @@
 class UserInfo {
-    constructor(firstName, lastName, patronymic, street, houseNumber, phoneNumber) {
-        this.firstName = firstName;
+    constructor(lastName = "", firstName = "", patronymic = "", streetName = "", houseNumber = "", phoneNumber = "") {
         this.lastName = lastName;
+        this.firstName = firstName;
         this.patronymic = patronymic;
-        this.street = street;
+        this.streetName = streetName;
         this.houseNumber = houseNumber;
         this.phoneNumber = phoneNumber
     }
+
+    equal(obj) {
+        return this.lastName == obj.lastName && this.firstName == obj.firstName && this.patronymic == obj.patronymic && this.streetName == obj.streetName && this.houseNumber == obj.houseNumber && this.phoneNumber == obj.phoneNumber;
+    }
 }
 
+const familyDoctorsInfo = {
+    "Тезер Аліна Іванівна": {
+        patients: [new UserInfo("Кефіров", "Олег", "Андрійович", "Чудова", "16Б", "+380661587452")],
+        workSchedule: {
+            "30.11": ["10:10", "12:30", "15:45", "17:00"],
+            "01.12": ["9:30", "14:30"]
+        }
+    }
+}
 
-
-// const family_doctors_info = {
-//     : {
-//         "лікар": "Тезер Аліна Олександрівна",
-//         "вільні-дати-і-часи": {
-//             "15.11": ["10:25", "12:25", "17:00"],
-//             "16.11": ["9:30", "14:45"]
-//         }
-//     }
-// };
-
+const form = document.querySelector(".form-container form");
 
 const formFields = {
     firstname: document.getElementById("firstname"),
     lastname: document.getElementById("lastname"),
-    street: document.getElementById("street"),
+    patronymic: document.getElementById("patronymic"),
+    streetName: document.getElementById("street"),
     houseNumber: document.getElementById("house-number"),
     familyDoctor: document.getElementById("family-doctor"),
     phoneNumber: document.getElementById("number"),
     date: document.getElementById("date"),
-    time: document.getElementById("time")
+    time: document.getElementById("time"),
+    complaint: document.getElementById("complaint")
 };
 
+// for (var field in formFields) {
+//     field.checkValidity();
+// }
 
-formFields.street.addEventListener("input", showSchedule);
-formFields.houseNumber.addEventListener("input", showSchedule);
+formFields.firstname.addEventListener("change", showDoctor);
+formFields.lastname.addEventListener("change", showDoctor);
+formFields.patronymic.addEventListener("change", showDoctor);
+formFields.streetName.addEventListener("change", showDoctor);
+formFields.houseNumber.addEventListener("change", showDoctor);
+formFields.phoneNumber.addEventListener("change", showDoctor);
+formFields.complaint.addEventListener("change", checkInvalid);
 
 const clear = document.getElementById("clear");
 clear.addEventListener("click", clearForm);
 
-const submit = document.querySelector("input[type=submit]");
-submit.addEventListener("click", submitForm);
+const submitButton = document.getElementById("record");
+submitButton.addEventListener("click", submitForm);
 
+function findDoctor(patient) {
+    for (let doctor in familyDoctorsInfo) {
+        for (let pat of familyDoctorsInfo[doctor].patients) {
+            if (pat.equal(patient)) {
+                return doctor;
+            }
+        }
+    }
+    return null;
+}
 
-function showSchedule(e) {
-    // if dict returns value after filling both fields
+const errSpan = document.createElement("span");
+errSpan.className = "err-msg";
+errSpan.innerText = "Поле не може бути порожнім";
+
+function showDoctor(e) {
+    console.log(e.target.parentNode);
+    checkInvalid(e);
     clearFields(formFields.time, formFields.date, formFields.familyDoctor);
-    var address = formFields.street.value + " " + formFields.houseNumber.value;
-    var doctorInfo = family_doctors_info[address.toLowerCase()];
+    const patient = new UserInfo(formFields.lastname.value,
+        formFields.firstname.value,
+        formFields.patronymic.value,
+        formFields.streetName.value,
+        formFields.houseNumber.value,
+        formFields.phoneNumber.value);
+    const doctor = findDoctor(patient);
     const plainText = document.createElement("span");
-    if (doctorInfo) {
-        plainText.innerText = doctorInfo["лікар"];
+    if (doctor) {
+        plainText.innerText = doctor;
         plainText.className = "doctor-block";
         formFields.familyDoctor.appendChild(plainText);
-        showDateTime(family_doctors_info, address);
+        showDateTime(doctor);
     } else {
         plainText.innerText = "не визначено";
         plainText.className = "unknown-block";
@@ -63,7 +96,14 @@ function showSchedule(e) {
         formFields.time.appendChild(plainText.cloneNode(true));
     }
 
+}
 
+function checkInvalid(event) {
+    if (!event.target.checkValidity() && !document.querySelector(`#${event.target.parentNode.id} .err-msg`)) {
+        event.target.parentNode.append(errSpan.cloneNode(true));
+    } else if (event.target.checkValidity() && document.querySelector(`#${event.target.parentNode.id} .err-msg`)) {
+        event.target.parentNode.removeChild(document.querySelector(`#${event.target.parentNode.id} .err-msg`));
+    }
 }
 
 function clearFields(...fields) {
@@ -74,8 +114,8 @@ function clearFields(...fields) {
     }
 }
 
-function showDateTime(obj, address) {
-    const workDatetimeObj = obj[address.toLowerCase()]["вільні-дати-і-часи"];
+function showDateTime(doctor) {
+    const workDatetimeObj = familyDoctorsInfo[doctor].workSchedule;
     for (var date in workDatetimeObj) {
         // для кожної дати з об'єкта створюємо відповідну кнопку та додаємо у блок дат
         const dateBtn = document.createElement("input");
@@ -87,7 +127,7 @@ function showDateTime(obj, address) {
         dateBtn.addEventListener("click", showTime);
     }
 
-    function showTime(e) {
+    function showTime(e = null) {
         clearFields(formFields.time);
         clearIds(document.getElementsByClassName(e.target.className));
         e.target.id = "active-date";
@@ -115,31 +155,45 @@ function showDateTime(obj, address) {
 
 function submitForm(e) {
     e.preventDefault();
-    for (var key in formFields) {
-        if (formFields[key].innerText == "") {
-            formFields[key].style.backgroundColor = "#f28585";
-            return;
+    showModal();
+    const date = document.querySelector(`#date #active-date`).value.split(".").map((elem) => Number(elem));
+    const time = document.querySelector(`#time #active-time`).value.split(":").map((elem) => Number(elem));
+    console.log(date, time);
+    const obj = JSON.stringify({
+        firstName: formFields.firstname.value,
+        lastName: formFields.lastname.value,
+        patronymic: formFields.patronymic.value,
+        streetName: formFields.streetName.value,
+        houseNumber: formFields.houseNumber.value,
+        familyDoctor: formFields.familyDoctor.innerText,
+        phoneNumber: formFields.phoneNumber.value,
+        dateTime: new Date(2022, date[1] - 1, date[0], time[0] + 2, time[1]),
+        complaint: document.getElementById("complaint").value
+    });
+
+    function showModal() {
+        const modal = document.getElementById("modal-wrapper");
+        const closeBtn = document.getElementById("close-button");
+        const currPos = window.scrollY;
+        modal.style.display = "flex";
+        modal.style.top = `calc(${currPos}px + 40vh)`;
+        window.addEventListener("scroll", disableScroll);
+        closeBtn.addEventListener("click", closeModal);
+
+        function closeModal() {
+            window.removeEventListener("scroll", disableScroll);
+            modal.style.display = "none";
+        }
+
+        function disableScroll() {
+            document.body.style.overflow = "hidden";
         }
     }
-    // const date = formFields.date.value.split(".").map((elem) => Number(elem));
-    // const time = formFields.time.getElementById("active-time").value.split(":").map((elem) => Number(elem));
-    // console.log(date, time);
-    // e.preventDefault();
-    // const obj = JSON.stringify({
-    //     firstName: formFields.firstname.value,
-    //     lastName: formFields.lastname.value,
-    //     street: formFields.street.value,
-    //     house: formFields.street.value,
-    //     familyDoctor: formFields.familyDoctor.innerText,
-    //     phoneNumber: formFields.phoneNumber.value,
-    //     dateTime: new Date(2022, date[1] - 1, date[0], time[0], time[1]),
-    //     complaint: document.getElementById("complaint").value
-    // });
-    // console.log(obj);
+
+    console.log(obj);
 }
 
 function clearForm(e) {
-
     // go through all inner-boxes
     e.preventDefault();
     for (var innerBox of document.getElementsByClassName("inner-box")) {
@@ -147,9 +201,8 @@ function clearForm(e) {
         console.log(lastElem);
         if (lastElem.tagName == "INPUT" || lastElem.tagName == "TEXTAREA") {
             lastElem.value = "";
-        } else if (lastElem.tagName = "SPAN") {
-            lastElem.innerText = "";
         }
+        showDoctor();
     }
     for (var label of unknownBlocks) {
         label.innerText = "не визначено";
